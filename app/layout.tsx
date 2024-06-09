34e257330f089c8ca41a5ad794120e7e9f9ebea1
AmrAbdelhady1 <37872476+AmrAbdelhady1@users.noreply.github.com>
2024-06-09 15:03:33 +0300

    add main header to the parent layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"
-import { cn } from "@/lib/utils"
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
 import "./globals.css";
+import MainHeader from "@/components/main-header";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: "TechnoVerse",
@@ -22,10 +23,13 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen bg-background font-sans antialiased px-[8vw]",
           fontSans.variable
         )}
-      >{children}</body>
+      >
+        <MainHeader />
+        {children}
+      </body>
     </html>
   );
 }
